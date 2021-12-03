a9de0284c5e13e0e7400cd0a5148f75842e1d8bf
Dmitry Velichko <soleil-2005@mail.ru>
2021-12-03 09:36:05 +0300

    New commit

--- app/js/script.js
+++ app/js/script.js
@@ -4,9 +4,9 @@ const header = document.querySelector('.header')
 btnHamburger.addEventListener('click', function () {
   console.log('click burger');
 
-  if (btnHamburger.classList.contains('open')) {
-    btnHamburger.classList.remove('open');
+  if (header.classList.contains('open')) {
+    header.classList.remove('open');
   } else {
-    btnHamburger.classList.add('open');
+    header.classList.add('open');
   }
 });
