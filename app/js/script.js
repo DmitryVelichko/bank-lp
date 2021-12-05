c6b7fb17a4d7963adc4e58b802ebdafbf769eea2
Dmitry Velichko <soleil-2005@mail.ru>
2021-12-05 10:22:02 +0300

    New commit

--- app/js/script.js
+++ app/js/script.js
@@ -8,8 +8,10 @@ btnHamburger.addEventListener('click', function () {
 
   if (header.classList.contains('open')) { // Close Hamburger Menu
     header.classList.remove('open');
-    overlay.classList.remove('fade-in');
-    overlay.classList.add('fade-out');
+    fadeElems.forEach(function(element) {
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    })
   } else { // Open Hamburger Menu
     header.classList.add('open');
     fadeElems.forEach(function(element) {
