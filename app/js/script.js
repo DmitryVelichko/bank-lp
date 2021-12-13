2edfa2496367c5e1f4837dab8a12f7f48243fc0e
Dmitry Velichko <soleil-2005@mail.ru>
2021-12-13 21:55:00 +0300

    New commit

--- app/js/script.js
+++ app/js/script.js
@@ -7,20 +7,21 @@ const fadeElems = document.querySelectorAll('.has-fade');
 btnHamburger.addEventListener('click', function () {
   console.log('click burger');
 
-  if (header.classList.contains('open')) { // Close Hamburger Menu
+  if (header.classList.contains('open')) {
+    // Close Hamburger Menu
     body.classList.remove('noscroll');
     header.classList.remove('open');
-    fadeElems.forEach(function(element) {
+    fadeElems.forEach(function (element) {
       element.classList.remove('fade-in');
       element.classList.add('fade-out');
-    })
-  } else { // Open Hamburger Menu
+    });
+  } else {
+    // Open Hamburger Menu
     body.classList.add('noscroll');
     header.classList.add('open');
-    fadeElems.forEach(function(element) {
+    fadeElems.forEach(function (element) {
       element.classList.remove('fade-out');
       element.classList.add('fade-in');
-    })
-   
+    });
   }
 });
